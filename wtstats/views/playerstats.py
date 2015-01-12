b93dbeb92afb47ac55629c3d48cdb46b7ba25582
Christian Stich <c68r69s@gmail.com>
2015-01-12 22:07:20 +0100

    more player stats

--- wtstats/views/playerstats.py
+++ wtstats/views/playerstats.py
@@ -5,8 +5,10 @@ from pyramid.view import (
 
 import math
 from sqlalchemy import func
-from wtstats.models import DBSession, Player, City, Tip, ValueType
+from wtstats.models import DBSession, Player, City, Tip, ValueType, Measurement
 from wtstats.models.tipvalue import TipValue
+
+import pandas as pd
 
 @view_defaults(permission='view')
 class PlayerStats:
@@ -43,22 +45,80 @@ class PlayerStats:
 		stat_types = DBSession.query(ValueType).all()
 		tips_total= len(player.tips)
 		
-		avg_points_all = DBSession.query(func.avg(TipValue.points)).first()[0]
-		avg_points_player = DBSession.query(func.avg(TipValue.points)).join(Tip, TipValue.tip).join(Player, Tip.player).filter(Player.id == player.id).first()[0]
 		avg_points_per_stat_all = {}
 		avg_points_per_stat_player = {}
 		
 		for city in cities:
 			city_stat_all = {}
 			city_stat_player = {}
+			
+			player_tips = DBSession.query(Tip.date, ValueType.name, TipValue.value, TipValue.points, TipValue.diff)\
+			                       .join(ValueType, TipValue.valuetype)\
+			                       .join(Tip, TipValue.tip)\
+			                       .join(City, Tip.city)\
+			                       .join(Player, Tip.player)\
+			                       .filter(Player.id == player.id, City.id == city.id).all()
+
+			if len(player_tips) == 0:
+				continue
+			
+			measurements = DBSession.query(Measurement.date, ValueType.name, func.avg(TipValue.value))\
+			                        .join(ValueType, TipValue.valuetype)\
+			                        .join(Measurement, TipValue.measurement)\
+			                        .join(City, Measurement.city)\
+			                        .group_by(Measurement.date, ValueType.id)\
+			                        .filter(City.id == city.id).all()
+
+			all_tips = DBSession.query(Tip.date, ValueType.name, func.avg(TipValue.value), func.avg(TipValue.points), func.avg(TipValue.diff))\
+			                       .join(ValueType, TipValue.valuetype)\
+			                       .join(Tip, TipValue.tip)\
+			                       .join(City, Tip.city)\
+			                       .group_by(Tip.date, ValueType.id)\
+			                       .filter(City.id == city.id).all()
+
+			measurements_dates, measurements_types, measurements_values = zip(*measurements)
+			md = pd.DataFrame({
+				'date': measurements_dates,
+				'type': measurements_types,
+				'value_measured': measurements_values
+			})
+			
+			all_dates, all_types, all_values, all_points, all_diff = zip(*all_tips)
+			ad = pd.DataFrame({
+				'date': all_dates,
+				'type': all_types,
+				'all_value': all_values,
+				'all_points': all_points,
+				'all_diff': all_diff,
+			})
+			
+			player_dates, player_types, player_values, player_points, player_diff = zip(*player_tips)
+			pld = pd.DataFrame({
+				'date': player_dates,
+				'type': player_types,
+				'player_value': player_values,
+				'player_points': player_points,
+				'player_diff': player_diff,
+			})
+			
+			data = pd.merge(md, pld, on=['date', 'type'])
+			data = pd.merge(data, ad, on=['date', 'type'])
+			
 			for stat in stat_types:
-				player_stats = DBSession.query(func.avg(TipValue.points)).join(Tip, TipValue.tip).join(City, Tip.city).join(Player, Tip.player).filter(Player.id == player.id, TipValue.valuetype_id == stat.id, Tip.city_id == city.id).first()[0]
-				if not player_stats:
-					break
+				stat_data = data[data.type == stat.name]
+				data_player = {
+					'points_avg': stat_data.player_points.mean(),
+					'diff_avg': stat_data.player_diff.mean(),
+				}
 				
-				city_stat_player[stat.name] = player_stats
-				city_stat_all[stat.name] = DBSession.query(func.avg(TipValue.points)).join(Tip, TipValue.tip).join(City, Tip.city).filter(TipValue.valuetype_id == stat.id, Tip.city_id == city.id).first()[0]
+				data_all = {
+					'points_avg': stat_data.all_points.mean(),
+					'diff_avg': stat_data.all_diff.mean(),
+				}
 				
+				city_stat_player[stat.name] = data_player
+				city_stat_all[stat.name] = data_all
+
 			
 			avg_points_per_stat_all[city.name] = city_stat_all
 			avg_points_per_stat_player[city.name] = city_stat_player
@@ -68,8 +128,6 @@ class PlayerStats:
 			'player': player,
 			'tips_total': tips_total,
 			'stat_types': stat_types,
-			'avg_points_player': avg_points_player,
-			'avg_points_all': avg_points_all,
 			'avg_points_per_stat_all': avg_points_per_stat_all,
 			'avg_points_per_stat_player': avg_points_per_stat_player, 
 		}
